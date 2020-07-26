30e69a0525023546ac7436ab7101cd672a818c8f
chaya2z <itunano57@gmail.com>
2020-07-27 03:23:07 +0900

    Add: Code hero-header & nav-bar on index.js.
    Why: Set title, hero-img, nav-bar, & footer.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <div className="titleSection">
+        <h1 className="websiteTitle">海底タランテラ</h1>
+      </div>
+      <div className="navBarSection">
+        <NavBar/>
+      </div>
+      <div className="contentsSection">
+      </div>
+      <Footer/>
+    </div>
+  )
 }
