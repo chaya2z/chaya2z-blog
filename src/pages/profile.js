172fb0f1bf6c7981e0f0794e66f136a326f38c1a
chaya2z <itunano57@gmail.com>
2020-07-29 01:48:36 +0900

    Add: Create Profile page.
    Why: Desc my bio.

---
+++ src/pages/profile.js
@@ -0,0 +1,41 @@
+import React from "react"
+import NavBar from "../components/nav-bar"
+import Footer from "../components/footer"
+import "./reset.css"
+import "./profile.css"
+
+class ProfileImg extends React.Component{
+  render() {
+    return (
+      <div className={'profileImg'}>
+        <img src={require("../images/profileBackImg.png")} alt={"profile header"}/>
+      </div>
+    )
+  }
+}
+
+class ProfileIcon extends React.Component{
+  render() {
+    return (
+      <div className={'profileIcon'}>
+        <img className={'iconImg'} src={require("../images/icon.jpg")} alt={"profile icon"}/>
+      </div>
+    )
+  }
+}
+
+export default function Home() {
+  return (
+    <div>
+        <NavBar/>
+      <div className="profileSection">
+        <ProfileImg/>
+        <ProfileIcon/>
+        <div className={"textContents"}>
+          <p>この記事では画像サイズ・文字の大きさ・文字の種類といったデザインを確認できます。この記事では画像サイズ・文字の大きさ・文字の種類といったデザインを確認できます。この記事では画像サイズ・文字の大きさ・文字の種類といったデザインを確認できます。この記事では画像サイズ・文字の大きさ・文字の種類といったデザインを確認できます。この記事では画像サイズ・文字の大きさ・文字の種類といったデザインを確認できます。この記事では画像サイズ・文字の大きさ・文字の種類といったデザインを確認できます。</p>
+        </div>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
