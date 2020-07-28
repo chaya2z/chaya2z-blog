3e784af1552a35548b19b904a7deff9ba4058813
chaya2z <itunano57@gmail.com>
2020-07-29 02:58:45 +0900

    Add: profile page
    Why: Visitor can contact me.

---
+++ src/pages/contact.js
@@ -0,0 +1,20 @@
+import React from "react"
+import NavBar from "../components/nav-bar"
+import Footer from "../components/footer"
+import "./reset.css"
+import "./contact.css"
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar/>
+      <div className={"contactForm"}>
+        <iframe
+          title="googleForm" src="https://docs.google.com/forms/d/e/1FAIpQLSdlcpfWL_hKSpToao2xzi6zSCsZtZ0_Njn0kCtXZ4vijbPlYw/viewform?embedded=true"
+          width="800" height="876" frameBorder="0" marginHeight="0" marginWidth="0">Loading…
+        </iframe>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
