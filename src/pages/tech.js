1867c485ec27716659c626f7a2d53177f31734fb
chaya2z <itunano57@gmail.com>
2020-08-23 09:07:36 +0900

    Add: Load data of blog page with GraphQL
    Why: Display blog card

--- src/pages/tech.js
+++ src/pages/tech.js
@@ -1,57 +1,73 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Image from "gatsby-image"
 import NavBar from "../components/nav-bar"
 import Footer from "../components/footer"
 import "./reset.css"
 import "./list.css"
 
-class TagTable extends React.Component {
-  render() {
-    return (
-      <div className={"tagTable"}>
-        <div className={'tag'}>Tag</div>
-        <div className={'tagItem'}>プログラミング</div>
-        <div className={'tagItem'}>ネットワーク</div>
-        <div className={'tagItem'}>OS/低レイヤ</div>
-        <div className={'tagItem'}>WWW</div>
-        <div className={'tagItem'}>その他</div>
-      </div>
-    )
-  }
-}
-
-class ArticleCard extends React.Component {
-  render() {
-    return (
-      <div className="articleCardContents">
-        <img className="thumbnail" src={require("../images/test.png")} alt={"thumbnail"}/>
-        <div className="textArea">
-          <h2 className={'cardTitle'}>ホームページ作成まとめ その1</h2>
-          <p className={'cardText'}>このホームページを作成過程をまとめました。その1ではデザインの作成までをまとます。目次 1. 目的 2. 要件定義 ....</p>
-        </div>
-      </div>
-    )
-  }
-}
-
-export default function Home() {
+export default ({ data }) => {
+  const edges = data.allMarkdownRemark.edges
   return (
-    <div style={{backgroundColor: "#F5F5F5"}}>
+    <div>
       <div className="titleSection">
-        <h1 className={'title'}>技術記事一覧</h1>
+        <h1 className={"title"}>技術記事一覧</h1>
       </div>
       <div className="navBarSection">
         <NavBar/>
       </div>
       <TagTable/>
       <div className="contentsContainer">
-        <ArticleCard/>
-        <ArticleCard/>
-        <ArticleCard/>
-        <ArticleCard/>
-        <ArticleCard/>
-        <ArticleCard/>
+        {edges.map((edge) => (
+          <div key={edge.node.date} className="articleCardContents">
+            <Image
+              fluid={edge.node.frontmatter.image.childImageSharp.fluid}
+            />
+            <div className="textArea">
+              <h2 className={"cardTitle"}>{edge.node.frontmatter.title}</h2>
+              <p className={"cardText"}>{edge.node.frontmatter.date}</p>
+            </div>
+          </div>
+        ))}
       </div>
       <Footer/>
     </div>
   )
 }
+
+class TagTable extends React.Component {
+  render() {
+    return (
+      <div className={"tagTable"}>
+        <div className={"tag"}>Tag</div>
+        <div className={"tagItem"}>プログラミング</div>
+        <div className={"tagItem"}>ネットワーク</div>
+        <div className={"tagItem"}>OS/低レイヤ</div>
+        <div className={"tagItem"}>WWW</div>
+        <div className={"tagItem"}>その他</div>
+      </div>
+    )
+  }
+}
+
+export const query = graphql`
+query {
+  allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}) {
+    edges {
+      node {
+        frontmatter {
+          date(formatString: "MM/DD YYYY")
+          title
+          image {
+            childImageSharp {
+              fluid(maxHeight: 600, maxWidth: 1280) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
