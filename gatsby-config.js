d4319b94b24290cffae0b269ad6c97e3b6fe2a65
chaya2z <itunano57@gmail.com>
2020-08-23 09:00:25 +0900

    Add: some plugin's config
    Why: to use plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,72 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const path = require(`path`)
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    // Used for the site title and SEO
+    title: `My Blog Title`,
+    // Used to provide alt text for your avatar
+    author: `My Name`,
+    // Used for SEO
+    description: `My site description...`,
+    // Used for resolving images in social cards
+    siteUrl: `https://example.com`,
+    // Used for social links in the root footer
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/chaya2z`
+      },
+      {
+        name: `GitHub`,
+        url: `https://github.com/gatsbyjs`
+      }
+    ]
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/blog`
+        // preset: false
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1000,
+              quality: 90,
+              linkImagesToOriginal: true,
+            }
+          }
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
+  ]
 }
