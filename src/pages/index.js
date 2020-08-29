2e53f91db4c1594dd35421be8db2e7ba19584d08
chaya2z <itunano57@gmail.com>
2020-08-29 16:35:17 +0900

    Add: homepage contents
    Why: description this blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 import NavBar from "../components/nav-bar"
 import Footer from "../components/footer"
 import "./reset.css"
 import "./index.css"
+import Image from "gatsby-image/index"
 
-export default function Home() {
+export default ({ data }) => {
+  const edges = data.allMarkdownRemark.edges
   return (
     <div>
       <div className="heroImgSection">
@@ -13,9 +16,65 @@ export default function Home() {
       <div className="navBarSection">
         <NavBar/>
       </div>
-      <div className="contentsSection">
+      <div className="indexContentsContainer">
+        <h2>このサイトについて</h2>
+        <div className={"siteDescription"}>
+          <p>この記事では画像サイズ・文字の大きさ・文字の種類といったデザインを確認できます。この記事では画像サイズ・文字の大きさ・文字の種類といったデザインを確認できます。この記事では画像サイズ・文字の大きさ・文字の種類といったデザインを確認できます。この記事では画像サイズ・文字の大きさ・文字の種類といったデザインを確認できます。この記事では画像サイズ・文字の大きさ・文字の種類といったデザインを確認できます。この記事では画像サイズ・文字の大きさ・文字の種類といったデザインを確認できます。</p>
+        </div>
+        <div className={"latestPostContainer"}>
+          <h2>最近の投稿</h2>
+          {edges.map((edge) => (
+            <div key={edge.node.date}>
+              <Link to={edge.node.fields.slug}>
+                <div className={"gridParent"}>
+                  <div className={"thumbnailItem"}>
+                    <Image
+                      fluid={edge.node.frontmatter.image.childImageSharp.fluid}
+                    />
+                  </div>
+                  <div className={"textItem"}>
+                    <h3>{edge.node.frontmatter.title}</h3>
+                    <p>{edge.node.excerpt}</p>
+                    <p className={"nextArticleText"}>記事を読む</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div className={"morePostsButton"}>
+          <p>その他の投稿を見る</p>
+        </div>
       </div>
       <Footer/>
     </div>
   )
 }
+
+export const LatestPageQuery = graphql`
+    query LatestPageQuery {
+        allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}, limit: 4) {
+            edges {
+                node {
+                    excerpt(format: PLAIN, pruneLength: 100, truncate: true)
+                    fields {
+                        slug
+                    }
+                    frontmatter {
+                        title
+                        date(formatString: "MM/DD YYYY")
+                        tags
+                        image {
+                            childImageSharp {
+                                fluid(maxWidth: 600, maxHeight: 315) {
+                                    ...GatsbyImageSharpFluid
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
+
