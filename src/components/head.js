f7f60cbeafb65cd3235f9d1bbd82ed8cbe1ac080
chaya2z <itunano57@gmail.com>
2020-08-31 05:12:02 +0900

    Add: google font
    Why: using in policy page

--- src/components/head.js
+++ src/components/head.js
@@ -4,8 +4,7 @@ import Helmet from "react-helmet"
 export default (props) => (
   <Helmet
     link={[
-      { href: "https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@600&display=swap", rel: "stylesheet" },
-      { href: "https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100&display=swap", rel: "stylesheet" }
+      { href: "https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100&family=Noto+Serif+JP:wght@300&family=Sawarabi+Gothic&display=swap", rel: "stylesheet" }
     ]}
   />
 )
