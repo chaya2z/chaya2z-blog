604ce9bbdd4d5de09db01675c7be5d94021b435b
chaya2z <itunano57@gmail.com>
2020-09-01 12:02:40 +0900

    Add: Google Analytics config
    Why: using GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,15 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-154213444-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
