13e07371b38d192903f9474e2d89b823c58441d4
chaya2z <itunano57@gmail.com>
2020-09-01 21:06:34 +0900

    Update: create post page config
    Why: clear page path

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,4 @@
 const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
-
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    })
-  }
-}
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
@@ -20,7 +7,7 @@ exports.createPages = async ({ graphql, actions }) => {
       allMarkdownRemark {
         edges {
           node {
-            fields {
+            frontmatter {
               slug
             }
           }
@@ -31,13 +18,13 @@ exports.createPages = async ({ graphql, actions }) => {
 
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
-      path: node.fields.slug,
+      path: node.frontmatter.slug,
       component: path.resolve(`./src/templates/blog-post.js`),
       context: {
         // Data passed to context is available
         // in page queries as GraphQL variables.
-        slug: node.fields.slug,
-      },
+        slug: node.frontmatter.slug
+      }
     })
   })
 }
