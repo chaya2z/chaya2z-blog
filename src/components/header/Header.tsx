904a70bb4533ed3a3249cb5f4b0e379b35d22af6
chaya2z <itunano57@gmail.com>
2021-02-17 14:35:17 +0900

    Add: header styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,22 @@
 import React from "react"
+import NavigationBar from "./navBar/NavigationBar"
+
+const style = {
+  backgroundColor: "gray",
+  paddingTop: "2rem",
+  paddingBottom: "2rem",
+  paddingLeft: "2rem",
+  color: "white"
+}
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <div>海底タランテラ</div>
-    </header>
+    <div>
+      <header style={style}>
+        <h1>海底タランテラ</h1>
+      </header>
+      <NavigationBar />
+    </div>
   )
 }
 
