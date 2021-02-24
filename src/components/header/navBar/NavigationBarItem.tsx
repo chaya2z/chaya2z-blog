6f5dec69f03291ca963480886449a2b8c67e0d98
chaya2z <itunano57@gmail.com>
2021-02-24 20:47:31 +0900

    Update: CSS

--- src/components/header/navBar/NavigationBarItem.tsx
+++ src/components/header/navBar/NavigationBarItem.tsx
@@ -1,15 +1,14 @@
 import React from "react"
-import { makeStyles } from "@material-ui/core"
 import { Link } from "gatsby"
 
-const useStyles = makeStyles(() => ({
+const styles: { [key: string]: React.CSSProperties} = {
   container: {
-    "paddingLeft": "2rem",
-    "paddingRight": "2rem",
-    "paddingTop": "1rem",
-    "paddingBottom": "1rem"
+    paddingLeft: "2rem",
+    paddingRight: "2rem",
+    paddingTop: "1rem",
+    paddingBottom: "1rem",
   }
-}))
+}
 
 interface Props {
   name: string
@@ -17,10 +16,9 @@ interface Props {
 }
 
 const NavigationBarItem: React.FC<Props> = ({ name, link }) => {
-  const classes = useStyles()
   return (
     <Link to={link}>
-      <div className={classes.container}>{name}</div>
+      <div style={styles.container}>{name}</div>
     </Link>
   )
 }
