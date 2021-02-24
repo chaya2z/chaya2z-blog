6b721eee5714b36630451f612ad3a4a949be2a68
chaya2z <itunano57@gmail.com>
2021-02-24 22:40:38 +0900

    Add: set header to center

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,29 @@
 import React from "react"
 import NavigationBar from "./navBar/NavigationBar"
 
-const style = {
-  backgroundColor: "gray",
-  paddingTop: "2rem",
-  paddingBottom: "2rem",
-  paddingLeft: "2rem",
-  color: "white",
-  fontFamily: '"M PLUS 1p", sans-serif'
+const styles: { [key: string]: React.CSSProperties } = {
+  siteTitle: {
+    backgroundColor: "gray",
+    paddingTop: "2rem",
+    paddingBottom: "2rem",
+    paddingLeft: "2rem",
+    color: "white",
+    fontFamily: "\"M PLUS 1p\", sans-serif"
+  },
+  container: {
+    maxWidth: "1080px",
+    marginLeft: "auto",
+    marginRight: "auto"
+  }
 }
 
 const Header: React.FC = () => {
   return (
-    <div>
-      <header style={style}>
-        <h1>海底タランテラ</h1>
+    <div style={styles.container}>
+      <header>
+        <h1 style={styles.siteTitle}>海底タランテラ</h1>
       </header>
-      <NavigationBar />
+      <NavigationBar/>
     </div>
   )
 }
