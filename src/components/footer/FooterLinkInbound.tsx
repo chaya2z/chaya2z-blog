729f357d7f12394fba78afb5747058403e18b511
chaya2z <itunano57@gmail.com>
2021-03-11 15:06:01 +0900

    Fix: link path

--- src/components/footer/FooterLinkInbound.tsx
+++ src/components/footer/FooterLinkInbound.tsx
@@ -5,8 +5,8 @@ const FooterLinkInbound: React.FC = () => {
   return (
     <div className="flex justify-center py-2">
       <Link to={"/"} className="border-r border-l w-48 hover:text-blue-500 duration-200 ease-in">ホーム</Link>
-      <Link to={"policy"} className="border-r w-48 hover:text-blue-500 duration-200 ease-in">サイトポリシー</Link>
-      <Link to={"contact"} className="border-r w-48 hover:text-blue-500 duration-200 ease-in">お問い合わせ</Link>
+      <Link to={"/policy"} className="border-r w-48 hover:text-blue-500 duration-200 ease-in">サイトポリシー</Link>
+      <Link to={"/contact"} className="border-r w-48 hover:text-blue-500 duration-200 ease-in">お問い合わせ</Link>
     </div>
   )
 }
