61e1f49ab09f07b3bcd3ccc293d7c04e0bed526b
chaya2z <itunano57@gmail.com>
2021-03-11 23:16:31 +0900

    Update: tailwind CSS

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,27 +1,11 @@
 import React from "react"
 import NavigationBar from "./navBar/NavigationBar"
 
-const styles: { [key: string]: React.CSSProperties } = {
-  siteTitle: {
-    backgroundColor: "gray",
-    paddingTop: "2rem",
-    paddingBottom: "2rem",
-    paddingLeft: "2rem",
-    color: "white",
-    fontFamily: "\"M PLUS 1p\", sans-serif"
-  },
-  container: {
-    maxWidth: "1080px",
-    marginLeft: "auto",
-    marginRight: "auto"
-  }
-}
-
 const Header: React.FC = () => {
   return (
-    <div style={styles.container}>
-      <header>
-        <h1 style={styles.siteTitle}>海底タランテラ</h1>
+    <div className="max-w-screen-lg mx-auto">
+      <header style={{ backgroundColor: "gray" }}>
+        <h1 className="text-white text-4xl py-8 pl-8">海底タランテラ</h1>
       </header>
       <NavigationBar/>
     </div>
